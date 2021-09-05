b6281134744b89d39dd48c6fff2c63cf49eaccf8
Diego Vicuna <diegojeanpierre98@gmail.com>
2021-09-05 17:45:23 -0500

    Validaciones

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,11 +1,28 @@
 import { Router } from "express";
 const router = Router()
 import * as productsCtrl from '../controllers/products.controller'
+import { authJwt } from "../middlewares";
 
-router.get('/',productsCtrl.getProducts)
-router.post('/',productsCtrl.createProduct)
-router.get('/:productId',productsCtrl.getProductById)
-router.put('/:productId',productsCtrl.updateProductById)
-router.delete('/:productId',productsCtrl.deleteProductById)
+router.get("/", productsCtrl.getProducts);
+
+router.get("/:productId", productsCtrl.getProductById);
+
+router.post(
+  "/",
+  [authJwt.verifyToken, authJwt.isModerator],
+  productsCtrl.createProduct
+);
+
+router.put(
+  "/:productId",
+  [authJwt.verifyToken, authJwt.isModerator],
+  productsCtrl.updateProductById
+);
+
+router.delete(
+  "/:productId",
+  [authJwt.verifyToken, authJwt.isAdmin],
+  productsCtrl.deleteProductById
+);
 
 export default router;
